9ff4cebd3336c122e5ee0bc67541900b78367538
nmr97007 <55343518+nmr97007@users.noreply.github.com>
2019-11-18 18:51:25 +0530

    Update newaria.py

--- pyrobot/plugins/newaria.py
+++ pyrobot/plugins/newaria.py
@@ -10,6 +10,7 @@ import asyncio
 import io
 import os
 from pyrobot import COMMAND_HAND_LER
+from pyrogram import Client, Filters
 
 cmd = "aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800  --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true"
 EDIT_SLEEP_TIME_OUT = 6
