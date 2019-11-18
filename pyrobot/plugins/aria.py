1479b639621e9fed56bcc41014faf577e76fb003
nmr97007 <55343518+nmr97007@users.noreply.github.com>
2019-11-18 18:31:32 +0530

    Update aria.py

--- pyrobot/plugins/aria.py
+++ pyrobot/plugins/aria.py
@@ -204,7 +204,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 except:
                     pass
                 #
-                msg = f"\nDownloading File: `{downloading_dir_name}`"
+                msg = f"\nDownloading File: `{file.name}`"
                 msg += f"\nSpeed: {file.download_speed_string()} 🔽 / {file.upload_speed_string()} 🔼"
                 msg += f"\nProgress: {file.progress_string()}"
                 msg += f"\nTotal Size: {file.total_length_string()}"
