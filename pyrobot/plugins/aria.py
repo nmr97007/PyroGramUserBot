a71187cd8304a41b155177fd19c41a3b439ef1de
nmr97007 <55343518+nmr97007@users.noreply.github.com>
2019-11-18 16:25:42 +0530

    Update aria.py

--- pyrobot/plugins/aria.py
+++ pyrobot/plugins/aria.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
 import os
 import aria2p
 from pyrogram import Client, Filters
